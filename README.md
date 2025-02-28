4e7cf85f6aecb203c3b35cbfbaf864b0ea35ddfc
Ideadly-Skies <Obie.kal22@gmail.com>
2025-02-28 13:06:47 +0700

    feat: revamped technical assessment with proposed Excalidraw and ER diagram

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# DGW-Technical-Test
-Simple API Exercise Leveraging The Go Programming Language and Echo Framework
+# DGW-Technical-Test Requirements
+
+**This is a Revamped DGW Technical Test Repo which hosts the codebase for the following project requirements:**
+    
+- **Auth API** Feature which *utilizes username and password (hashed)* which will produce a *JWT Token*.
+- **Crud Features**: Could be accessed by Someone **Logged-In.**
+
+**Technical Requirement:**
+- API must be created using **Go** Programming Language
+- Database Utilizes **MySQL / PostgreSQL.**
+
+**Bonus Points (Addressed):**
+- Utilizes **clean architecture**
+- Utilizes **Dependency Injection** 
+- Utilizes **HTTP Framework**
+- Provides **API Documentation (Swagger Docs)**
+- Create a **Simple UI with web frameworks** such as React/Vue etc. (TBA)
+
+# Features
+
+
+# Documentation
+
+This project's documentation could be accessed viaq
+
+### Diagrams
+
+- The project's diagram could be accessed via this link: https://excalidraw.com/#json=RHZI-AHqPudVYEVp9ZHV9,Y-YWt53YGqxADlwC5NKJwg
--- main.go
+++ main.go
@@ -2,27 +2,13 @@ package main
 
 import (
 	config "dgw-technical-test/config/database"
-	_ "dgw-technical-test/docs"
-	admin_handler "dgw-technical-test/internal/adminHandler"
-	cust_middleware "dgw-technical-test/internal/middleware"
-	product_handler "dgw-technical-test/internal/productHandler"
+	// cust_middleware "dgw-technical-test/internal/middleware"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	echoSwagger "github.com/swaggo/echo-swagger"
 )
 
-// @title FTGO PlasCash Project
-// @version 1.0
-// @description API documentation for the FTGO PlashCash project.
-// @termsOfService http://example.com/terms/
-// @contact.Obie API Support
-// @contact.url www.linkedin.com/in/obie-ananda-a87a64212
-// @contact.email Obie.kal22@gmail.com
-// @license.name MIT
-// @license.url http://opensource.org/licenses/MIT
-// @host localhost:8080
-// @BasePath /
 func main() {
 	// migrate data to supabase
 	// config.MigrateData()
@@ -39,20 +25,6 @@ func main() {
 
 	// Swagger route
 	e.GET("/swagger/*", echoSwagger.WrapHandler)
-
-	// public routes
-	e.POST("/admin/register", admin_handler.RegisterAdmin)
-	e.POST("/admin/login", admin_handler.LoginStoreAdmin)
-
-	// protected routes for store admin using JWT middleware
-	AdminGroup := e.Group("/admin")
-	AdminGroup.Use(cust_middleware.JWTMiddleware)
-
-	// Route for Admin
-	AdminGroup.GET("/products", product_handler.GetAllProducts)
-	AdminGroup.POST("/create-product", product_handler.CreateProduct)
-	AdminGroup.PUT("/products/:id", product_handler.UpdateProduct)
-	AdminGroup.DELETE("/products/:id", product_handler.DeleteProduct)
 
 	// start the server at 8080
 	e.Logger.Fatal(e.Start(":8080"))
